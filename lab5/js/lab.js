30ca23f09146185ddf32983df3323becefe8fa61
DarkDragonair <darkdragonair135@gmail.com>
2023-04-26 15:24:04 -0700

    Lab 5

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,21 +1,27 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+/* 
+lab.js - Variables
+Summary.
+- Practice basic variables.
+Description.
+- Declaring and calling variables in relation to a car.
+Author: Jessica Jorgensen
+Date: 4/26/23
+*/
 
-// Constants
+// Declare
+make = "Volkswagon";
+model = "Polo";
+color = "White";
+year = 2009;
+ownIt = false;
 
-// Functions
+// Calculation
+age = 2023 - year;
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+// Print
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Owned: " + ownIt + "<br>");
+document.writeln("Age: " + age + " years<br>");
