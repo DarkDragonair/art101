fff4449e3bec3fa039356137535c06c5e612e359
DarkDragonair <darkdragonair135@gmail.com>
2023-05-29 23:47:44 -0700

    12, 3

--- lab12/js/lab.js
+++ lab12/js/lab.js
@@ -12,49 +12,65 @@ Date: 05/22/23
 // - Element "button" assigned to a variable.
 var buttonName = document.getElementById("button");
 
+const description = 2;
+
 // Function Declaration
 // - A simple function that takes a string, uses modulus, and returns a Hogwarts House's name.
-function sortingHat(str) {
+function sortingHat(list, name) {
   // Variable Declaration - Function  
   // - Taking the length of the inputted String.
-  var length = str.length;
-  // - Modulus of 4 affecting length. Will be 0, 1, 2, or 3.
-  var mod = length % 4;
+  var length = name.length;
+  console.log(length);
 
-  // Nest
-  // - A mod of 0 will return the House name and description.
-  if (mod == 0) {
-    return ["Gryffindor", "With a color scheme of scarlet and gold, Gryffindor represents those that are chivalrous and valorous. A lion is the House's animal."];
+  // - Modulus of 4 affecting length. Will be 0, 1, 2, or 3.
+  var mod = length % list;
+  console.log(mod);
+
+  // - Variable Hogwarts house creation.
+  // -- Array for the houses.
+  var hogwarts = [list];
+
+  // -- Adds the description to each house, making it a two-dimensional array.
+  for (var i = 0; i < list; i++) {
+    hogwarts[i] = [description];
   }
-  // - Else if, in the case that it is not of the previous value(s).
-  else if (mod == 1) {
-    return ["Ravenclaw", "Those in Ravenclaw display a need to learn and sharpness, portrayed by an eagle. Their house colors are blue and bronze."];
-  }
-  else if (mod == 2) {
-    return ["Slytherin", "Clothed in green and silver, Slytherin's students are exemplary cases of ambition and craftiness - best represented by a snake."];
-  }
-  // - The final else if is not needed, because the only other Integer that could be declared after not being 0, 1, or 2 is 3.
-  else {
-    return ["Hufflepuff", "Hardworking and loyal to a fault, the House of Hufflepuff is always willing to lend others a hand. Their yellow and black color scheme is accented by their animal being a badger."];
-  }
+
+  console.log(hogwarts);
+
+  // - Standard four houses. Can be overwritten to change or add specific Houses.
+  var hogwarts = [
+    ["Gryffindor", "With a color scheme of scarlet and gold, Gryffindor represents those that are chivalrous and valorous. A lion is the House's animal."], 
+    ["Ravenclaw", "Those in Ravenclaw display a need to learn and sharpness, portrayed by an eagle. Their house colors are blue and bronze."],
+    ["Slytherin", "Clothed in green and silver, Slytherin's students are exemplary cases of ambition and craftiness - best represented by a snake."],
+    ["Hufflepuff", "Hardworking and loyal to a fault, the House of Hufflepuff is always willing to lend others a hand. Their yellow and black color scheme is accented by their animal being a badger."]
+  ];
+
+  console.log(hogwarts);
+
+  // Assigns the house and description
+  var sorted = [hogwarts[mod][0], hogwarts[mod][1]];
+  console.log("Assigned House: ", sorted[0]);
+  console.log("House Description: ", sorted[1]);
+
+  return sorted;
 }
 
 // Event Listener
-// - On button click, runs sortingHat() and 
+// - On button click, runs sortingHat().
 buttonName.addEventListener("click", function() {
   // Variable Declaration - Function
   // - Allocate the inputted name from the user.
+  var list = document.getElementById("number").value;
   var name = document.getElementById("input").value;
+
+  console.log("Entered houses: ", list);
   console.log("Entered name: ", name);
 
-  // - Set equal to the result of sortingHat() using name.
-  var house = sortingHat(name)[0];
-  var description = sortingHat(name)[1];
-  console.log("Assigned House: ", house);
-  console.log("House Description: ", description);
+  // - 
+  var output = sortingHat(list, name);
 
   // - Statement of the chosen House.
-  var result = "<p>The Sorting Hat has sorted you into " + house + ".<br>" + "<p>" + description;
+  var result = "<p>The Sorting Hat has sorted you into " + output[0] + ".<br>" + "<p>" + output[1];
 
   // Assigns the HTML of the <div> "output" as the result of sortingHat().
   document.getElementById("output").innerHTML = result;
